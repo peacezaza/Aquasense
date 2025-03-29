b549b992f588a043fa0b59f1c17627abffe033be
Kanisorn <32830583+peacezaza@users.noreply.github.com>
2025-03-29 13:13:22 +0700

    Update sketch_mar8a.ino

--- sketch_mar8a.ino
+++ sketch_mar8a.ino
@@ -7,17 +7,43 @@
 #define IN4 13
 #define L298IN1 21
 #define L298IN2 22
-#define PH_SENSOR_PIN 36
+#define oxygenQuailty 5
+#define pHQuailty 7
+#define turbidityQuailty 100
+#define STEPS_PER_REV 1024
+#define pHTopic "/pHSensor"
+#define oxygenTopic "/OxygenSensor"
+#define turbidityTopic "/TurbiditySensor"
 
+#include <WiFi.h>
+#include <PubSubClient.h>
 
-#define STEPS_PER_REV 2048 
-int stepDelay = 2;          
-int sensorValue = 0;
-float voltage = 0.0; 
-float phValue = 0.0; 
+#define MQTT_SERVER "40.81.22.116"
+#define MQTT_PORT 1883
+#define MQTT_USERNAME ""
+#define MQTT_PASSWORD ""
+#define MQTT_NAME "ESP32_1"
+WiFiClient client;
+PubSubClient mqtt(client);
+
+const char* ssid = "BBKK_2.4GHz";
+const char* password = "0800536270";
+
+float pHValue = 0;
+float oxygenValue = 0;
+float turbidity = 0;
+
+int currentTime = 0;
+int lastSaveTime = 0;
+
+int stepDelay = 2;
+
+void stepMotor(int step);
+void connectWiFi();
+void mqttConnect();
+void callback(char* topic, byte* payload, unsigned int length);
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(drivo1, OUTPUT);
   pinMode(drivo2, OUTPUT);
@@ -28,7 +54,56 @@ void setup() {
   pinMode(IN4, OUTPUT);
   pinMode(L298IN1, OUTPUT);
   pinMode(L298IN2, OUTPUT);
+  digitalWrite(drivo1, HIGH);
+  digitalWrite(drivo2, HIGH);
+  digitalWrite(drivo3, HIGH);
+
+  connectWiFi();
 }
+
+void loop() {
+  mqttConnect();
+  mqtt.loop();
+
+
+  currentTime = millis();
+  if (currentTime - lastSaveTime > 1000) {
+    for (int i = 0; i < STEPS_PER_REV; i++) {
+      stepMotor(i);
+      delay(stepDelay);
+    }
+    if (pHValue < pHQuailty - 1) {
+      digitalWrite(drivo2, LOW);
+      delay(300);
+      digitalWrite(drivo2, HIGH);
+    } else if (pHValue > pHQuailty + 1) {
+      digitalWrite(drivo1, LOW);
+      delay(300);
+      digitalWrite(drivo1, HIGH);
+    }
+    for (int i = STEPS_PER_REV - 1; i >= 0; i--) {
+      stepMotor(i);
+      delay(stepDelay);
+    }
+    if (oxygenValue < oxygenQuailty) {
+      digitalWrite(L298IN1, LOW);
+      digitalWrite(L298IN2, HIGH);
+    } else {
+      digitalWrite(L298IN1, LOW);
+      digitalWrite(L298IN2, LOW);
+    }
+    if (turbidity > turbidityQuailty) {
+      digitalWrite(drivo3, LOW);
+    } else {
+      digitalWrite(drivo3, HIGH);
+    }
+
+    lastSaveTime = currentTime;
+  }
+}
+
+
+
 void stepMotor(int step) {
   switch (step % 4) {
     case 0:
@@ -58,33 +133,62 @@ void stepMotor(int step) {
   }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(drivo1, LOW);
-  digitalWrite(drivo2, LOW);
-  digitalWrite(drivo3, LOW);
+void connectWiFi() {
+  WiFi.mode(WIFI_STA);  //Optional
+  WiFi.begin(ssid, password);
+  Serial.print("\nConnecting");
 
-  for (int i = 0; i < STEPS_PER_REV; i++) {
-    stepMotor(i);
-    delay(stepDelay);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
   }
-  
-  digitalWrite(L298IN1, LOW); 
-  digitalWrite(L298IN2, HIGH);  
-
-  // int sensorValue = analogRead(PH_SENSOR_PIN);  // Read the analog value
-  // float voltage = sensorValue * (5.0 / 4095.0);  // Convert the value to voltage (assuming 5V input)
-  
-  // // Assuming the sensor outputs 2.5V at pH 7, and 59mV change per pH unit
-  // float phValue = (voltage - 2.5) / 0.059;
-  
-  // // Output the sensor data
-  // Serial.print("Sensor Value: ");
-  // Serial.print(sensorValue);
-  // Serial.print("\tVoltage: ");
-  // Serial.print(voltage, 2);  // Display voltage with 2 decimal places
-  // Serial.print("\tPH: ");
-  // Serial.println(phValue, 2);  // Display pH value with 2 decimal places
-
-  delay(1000); 
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
+  mqtt.setServer(MQTT_SERVER, MQTT_PORT);
+  mqtt.setCallback(callback);
 }
+
+void mqttConnect() {
+  if (mqtt.connected() == false) {
+    Serial.print("MQTT connection... ");
+    if (mqtt.connect(MQTT_NAME, MQTT_USERNAME,
+                     MQTT_PASSWORD)) {
+      Serial.println("connected");
+      mqtt.subscribe("/OxygenSensor");
+      mqtt.subscribe("/TurbiditySensor");
+      mqtt.subscribe("/pHSensor");
+    } else {
+      Serial.println("failed");
+      delay(5000);
+    }
+  } else {
+    mqtt.loop();
+  }
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  payload[length] = '\0';
+  String topic_str = topic, payload_str = (char*)payload;
+  // Serial.print("Payload is :");
+  // Serial.println("[" + topic_str + "]: " + payload_str);
+  int value = payload_str.toInt();
+  if (topic_str == pHTopic) {
+    pHValue = value;
+    Serial.print("Payload is :");
+    Serial.println("[" + topic_str + "]: " + payload_str);
+  } else if (topic_str == oxygenTopic) {
+    oxygenValue = value;
+    Serial.print("Payload is :");
+    Serial.println("[" + topic_str + "]: " + payload_str);
+  } else if (topic_str == turbidityTopic) {
+    turbidity = value;
+    Serial.print("Payload is :");
+    Serial.println("[" + topic_str + "]: " + payload_str);
+  }
+}
+
+If I want to change my code to rtos 
+what should i use like task or queue 
+teach me don't do it for me
